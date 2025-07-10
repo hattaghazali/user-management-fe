72ff539f7902ad9fe5a7d53bce501b429a6a5cec
hattaghazali <hattaltd@gmail.com>
2025-07-11 00:12:02 +0800

    BUILD: Install new packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,16 @@
-import { AliasOptions, defineConfig } from 'vite';
 import path from 'path';
-import tsconfigPaths from 'vite-tsconfig-paths';
-const root = path.resolve(__dirname, 'src');
-
-import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+// import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), tsconfigPaths()],
-    // server: {
-    //     proxy: {
-    //         '/api': {
-    //             target: 'http://localhost:8989',
-    //             changeOrigin: true
-    //         }
-    //     }
-    // },
+    // plugins: [react(), tailwindcss(), tsconfigPaths()],
+    plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
-            '@': root
-        } as AliasOptions
+            '@': path.resolve(__dirname, './src')
+        }
     }
 });
